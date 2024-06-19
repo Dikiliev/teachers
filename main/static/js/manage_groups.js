2f39e378649135c11bad7dfa1589301e2ccc4087
Dikiliev <76054541+Dikiliev@users.noreply.github.com>
2024-06-20 02:58:16 +0300

    add and delete groups and schedules logic

--- main/static/js/manage_groups.js
+++ main/static/js/manage_groups.js
@@ -1,9 +1,18 @@
+
+let data = {};
+
 document.addEventListener('DOMContentLoaded', function () {
     manageGroupsInit().then();
 });
 
 async function manageGroupsInit(){
-    const data = await getData(user_id);
+    data = await getData(user_id);
+
+    refreshData();
+}
+
+function refreshData(){
+    console.log(data);
 
     const container = document.getElementById('groups-container');
     container.innerHTML = '';
@@ -20,9 +29,10 @@ async function manageGroupsInit(){
 
     container.innerHTML +=
         `<div class="group-fields">
-            <button class="button">Добавить новую группу</button>
-            <button class="button">Сохранить</button>
-        </div>`
+            <button onclick="addGroup()" class="button">Добавить новую группу</button>
+            <button class="button" type="submit">Сохранить</button>
+        </div>
+        `
 }
 
 async function getData(teacherId){
@@ -43,11 +53,11 @@ function generateGroupElement(group, groupIndex, available_subjects){
         <h2>Группа ${groupIndex + 1}</h2>
         <div class="group-fields">
             <div class="input-group">
-                <label for="name_${groupIndex}">Название группы:</label>
+                <label for="name_${groupIndex}">Название&nbsp;группы:</label>
                 <input type="text" id="name_${groupIndex}" name="name_${groupIndex}" value="${group.name}" required>
             </div>
             <div class="input-group">
-                <label for="price_${groupIndex}">Цена в месяц:</label>
+                <label for="price_${groupIndex}">Цена&nbsp;в&nbsp;месяц:</label>
                 <input type="number" id="price_${groupIndex}" name="price_${groupIndex}" value="${group.price}" required>
             </div>
             <div class="input-group">
@@ -61,7 +71,13 @@ function generateGroupElement(group, groupIndex, available_subjects){
             </div>
         </div>
         <div class="schedules-list"></div>
-        <button class="button add-schedule">Добавить новый урок</button>
+        
+        <div class="container row" style="width: 100%">
+            <div class="group-fields">
+                <button onclick="addSchedule(${groupIndex})" class="button add-schedule">Добавить урок</button>
+                <button onclick="deleteGroup(${groupIndex})" class="button danger delete-schedule">Удалить группу</button>
+            </div>
+        </div>
     `;
     return groupDiv;
 }
@@ -73,23 +89,50 @@ function generateScheduleElement(schedule, scheduleIndex, groupIndex, days_of_we
         <h3>Урок ${scheduleIndex + 1}</h3>
         <div class="schedule-fields">
             <div class="input-group">
-                <label for="day_of_week_${groupIndex}_${scheduleIndex}">День недели:</label>
+                <label for="day_of_week_${groupIndex}_${scheduleIndex}">День&nbsp;недели:</label>
                 <select id="day_of_week_${groupIndex}_${scheduleIndex}" name="day_of_week_${groupIndex}_${scheduleIndex}" required>
                     <option value="0"> -- Не выбрано -- </option>
                     ${days_of_week.map(day => `
-                        <option value="${day[0]}" ${day[0] === schedule.day_of_week ? 'selected' : ''}>${day[1]}</option>
+                        <option value="${day[0]}" ${day[1] === schedule.day_of_week ? 'selected' : ''}>${day[1]}</option>
                     `).join('')}
                 </select>
             </div>
             <div class="input-group">
-                <label for="start_time_${groupIndex}_${scheduleIndex}">Время начала:</label>
+                <label for="start_time_${groupIndex}_${scheduleIndex}">Время&nbsp;начала:</label>
                 <input type="time" id="start_time_${groupIndex}_${scheduleIndex}" name="start_time_${groupIndex}_${scheduleIndex}" value="${schedule.start_time}" required>
             </div>
             <div class="input-group">
-                <label for="duration_${groupIndex}_${scheduleIndex}">Длительность (в минутах):</label>
+                <label for="duration_${groupIndex}_${scheduleIndex}">Длительность&nbsp;(в&nbsp;минутах):</label>
                 <input type="number" id="duration_${groupIndex}_${scheduleIndex}" name="duration_${groupIndex}_${scheduleIndex}" value="${Math.round(schedule.duration_minutes)}" required>
             </div>
+            <button onclick="deleteSchedule(${groupIndex}, ${scheduleIndex})" class="button danger delete-schedule">Удалить урок</button>
         </div>
+        
+        <div class="container row" style="width: 100%"></div>
     `;
     return scheduleDiv;
+}
+
+function addGroup(){
+    const group = {
+        'schedules': [{},],
+        'subject': {}
+    }
+    data.groups.push(group)
+    refreshData();
+}
+
+function deleteGroup(groupIndex){
+    data.groups.splice(groupIndex, 1);
+    refreshData();
+}
+
+function addSchedule(groupIndex){
+    data.groups[groupIndex].schedules.push({})
+    refreshData();
+}
+
+function deleteSchedule(groupIndex, scheduleIndex){
+    data.groups[groupIndex].schedules.splice(scheduleIndex, 1);
+    refreshData();
 }
